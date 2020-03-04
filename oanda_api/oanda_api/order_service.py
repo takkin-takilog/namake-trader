6f400a8b8970f9871108abba0e983a32716ab6a5
takkin <takkin.takilog@gmail.com>
2020-03-05 00:07:28 +0900

    [update]

--- oanda_api/oanda_api/order_service.py
+++ oanda_api/oanda_api/order_service.py
@@ -1,5 +1,6 @@
 import rclpy
 from rclpy.node import Node
+from api_msgs.srv import OrderCreate
 
 
 class OrderService(Node):
@@ -10,6 +11,17 @@ class OrderService(Node):
         # Set logger lebel
         self.__logger = super().get_logger()
         self.__logger.set_level(rclpy.logging.LoggingSeverity.DEBUG)
+
+        # Create service "order_create"
+        srv_type = OrderCreate
+        srv_name = "order_create"
+        callback = self.__cb_order_create
+        self.order_create_srv = self.create_service(srv_type,
+                                                    srv_name,
+                                                    callback)
+
+    def __cb_order_create(self, request, response):
+        pass
 
 
 def main(args=None):
