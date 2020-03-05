66e0b5ec7f43a22c7b8738c46fa9338634f1fc7b
takkin <takkin.takilog@gmail.com>
2020-03-06 00:18:24 +0900

    [update]

--- oanda_api/oanda_api/order_service.py
+++ oanda_api/oanda_api/order_service.py
@@ -1,8 +1,9 @@
 import rclpy
 from rclpy.node import Node
-from api_msgs.srv import OrderCreate
+from api_msgs.srv import OrderCreateSrv
+from api_msgs.srv._order_create import Metaclass_OrderCreate_Request as OrderCreateReq
+from oandapyV20.endpoints.orders import OrderCreate
 from oandapyV20 import API
-from oandapyV20.exceptions import V20Error, StreamTerminated
 
 
 class OrderService(Node):
@@ -21,26 +22,61 @@ class OrderService(Node):
         self.declare_parameter(PRMNM_ACCOUNT_NUMBER)
         self.declare_parameter(PRMNM_ACCESS_TOKEN)
 
-        account_number = self.get_parameter(PRMNM_ACCOUNT_NUMBER).value
+        self.__account_number = self.get_parameter(PRMNM_ACCOUNT_NUMBER).value
         access_token = self.get_parameter(PRMNM_ACCESS_TOKEN).value
-        self.__logger.debug("[OANDA]Account Number:%s" % account_number)
+        self.__logger.debug("[OANDA]Account Number:%s" % self.__account_number)
         self.__logger.debug("[OANDA]Access Token:%s" % access_token)
 
         # Create service "order_create"
-        srv_type = OrderCreate
+        srv_type = OrderCreateSrv
         srv_name = "order_create"
         callback = self.__cb_order_create
         self.order_create_srv = self.create_service(srv_type,
                                                     srv_name,
                                                     callback)
 
-
         self.__api = API(access_token=access_token)
-
 
     def __cb_order_create(self, request, response):
 
+        data = {
+            "order": {
+                "type": request.type,
+            }
+        }
+
+        data_order = data["order"]
+
+        if (request.type == OrderCreateReq.TYP_LIMIT) or (request.type == OrderCreateReq.TYP_STOP):
+            tmp = {
+                "price": request.units,
+                "timeInForce": "GTC",
+            }
+            data_order.update(tmp)
+
+        tmp = {
+            "instrument": request.instrument,
+            "units": request.units,
+            "positionFill": "DEFAULT",
+                "takeProfitOnFill": {
+                    "timeInForce": "GTC",
+                    "price": request.take_profit_price
+                },
+                "stopLossOnFill": {
+                    "timeInForce": "GTC",
+                    "price": request.stop_loss_price
+                },
+        }
+        data_order.update(tmp)
+
+        ep = OrderCreate(accountID=self.__account_number, data=data)
+        rsp = self.__api.request(ep)
+
+        import json
+        print(json.dumps(rsp, indent=2))
+
         return response
+
 
 def main(args=None):
     rclpy.init(args=args)
