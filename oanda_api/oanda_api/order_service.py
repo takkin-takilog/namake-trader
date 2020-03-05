d1caa8f59b4387206ef45c323c2f3ff25b2e8602
takkin <takkin.takilog@gmail.com>
2020-03-05 20:34:18 +0900

    [update]

--- oanda_api/oanda_api/order_service.py
+++ oanda_api/oanda_api/order_service.py
@@ -1,6 +1,8 @@
 import rclpy
 from rclpy.node import Node
 from api_msgs.srv import OrderCreate
+from oandapyV20 import API
+from oandapyV20.exceptions import V20Error, StreamTerminated
 
 
 class OrderService(Node):
@@ -12,6 +14,18 @@ class OrderService(Node):
         self.__logger = super().get_logger()
         self.__logger.set_level(rclpy.logging.LoggingSeverity.DEBUG)
 
+        PRMNM_ACCOUNT_NUMBER = "account_number"
+        PRMNM_ACCESS_TOKEN = "access_token"
+
+        # Declare parameter
+        self.declare_parameter(PRMNM_ACCOUNT_NUMBER)
+        self.declare_parameter(PRMNM_ACCESS_TOKEN)
+
+        account_number = self.get_parameter(PRMNM_ACCOUNT_NUMBER).value
+        access_token = self.get_parameter(PRMNM_ACCESS_TOKEN).value
+        self.__logger.debug("[OANDA]Account Number:%s" % account_number)
+        self.__logger.debug("[OANDA]Access Token:%s" % access_token)
+
         # Create service "order_create"
         srv_type = OrderCreate
         srv_name = "order_create"
@@ -20,9 +34,13 @@ class OrderService(Node):
                                                     srv_name,
                                                     callback)
 
-    def __cb_order_create(self, request, response):
-        pass
 
+        self.__api = API(access_token=access_token)
+
+
+    def __cb_order_create(self, request, response):
+
+        return response
 
 def main(args=None):
     rclpy.init(args=args)
