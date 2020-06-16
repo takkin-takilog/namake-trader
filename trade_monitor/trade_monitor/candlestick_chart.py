d72e67c2db5d7d6e19b857251b58d4e626e74e2a
takkin <takkin.takilog@gmail.com>
2020-06-17 00:37:20 +0900

    [update]

--- trade_monitor/trade_monitor/candlestick_chart.py
+++ trade_monitor/trade_monitor/candlestick_chart.py
@@ -8,7 +8,7 @@ from PySide2.QtGui import QPalette, QColor, QFont, QPen, QFontMetrics, QPainter,
 from PySide2.QtGui import QLinearGradient
 from PySide2.QtWidgets import QGraphicsLineItem
 from trade_manager_msgs.msg import Granularity as Gran
-
+from trade_monitor.abstract import CalloutChartAbs, CandlestickChartAbs
 
 CALLOUT_PRICE_COLOR = QColor(204, 0, 51)
 CALLOUT_DATE_COLOR = QColor(0, 204, 51)
@@ -146,54 +146,7 @@ class CandlestickChart(object):
         self.__chartview.resize(frame_size)
 
 
-class CalloutAbs(QGraphicsItem):
-    __metaclass__ = ABCMeta
-
-    def __init__(self, parent: QtCharts.QChart):
-        super().__init__()
-        self._chart = parent
-        self._text = ""
-        self._anchor = QPointF()
-        self._font = QFont()
-        self._textRect = QRectF()
-        self._rect = QRectF()
-
-    @abstractmethod
-    def updateGeometry(self, text: str, point: QPointF):
-        raise NotImplementedError()
-
-    def boundingRect(self) -> QRectF:
-        from_parent = self.mapFromParent(self._anchor)
-        anchor = QPointF(from_parent)
-        rect = QRectF()
-        rect.setLeft(min(self._rect.left(), anchor.x()))
-        rect.setRight(max(self._rect.right(), anchor.x()))
-        rect.setTop(min(self._rect.top(), anchor.y()))
-        rect.setBottom(max(self._rect.bottom(), anchor.y()))
-        return rect
-
-    @abstractmethod
-    def paint(self,
-              painter: QPainter,
-              option: QStyleOptionGraphicsItem,
-              widget: QWidget):
-        raise NotImplementedError()
-
-    def _setText(self, text: str):
-        self._text = text
-
-        metrics = QFontMetrics(self._chart.font())
-        self._textRect = QRectF(metrics.boundingRect(QRect(0, 0, 0, 0),
-                                                     Qt.AlignLeft,
-                                                     self._text))
-        dx = 5
-        dy = 5
-        self._textRect.translate(dx, dy)
-        self.prepareGeometryChange()
-        self._rect = self._textRect.adjusted(-dx, -dy, dx, dy)
-
-
-class CalloutDataTime(CalloutAbs):
+class CalloutDataTime(CalloutChartAbs):
 
     def __init__(self, parent: QtCharts.QChart):
         super().__init__(parent)
@@ -213,11 +166,8 @@ class CalloutDataTime(CalloutAbs):
               painter: QPainter,
               option: QStyleOptionGraphicsItem,
               widget: QWidget):
-        #print("---------- paint ----------")
         path = QPainterPath()
-        mr = self._rect
-        #print("mr: {}" .format(mr))
-        path.addRoundedRect(mr, 5, 5)   # 丸みを帯びた長方形の角を規定
+        path.addRoundedRect(self._rect, 5, 5)   # 丸みを帯びた長方形の角を規定
 
         # 枠を描写
         painter.setBrush(CALLOUT_DATE_COLOR)    # 図形の塗りつぶし
@@ -225,11 +175,11 @@ class CalloutDataTime(CalloutAbs):
         painter.drawPath(path)
 
         # 文字を描写
-        painter.setPen(QPen(QColor("white")))
+        painter.setPen(QPen(QColor(Qt.white)))
         painter.drawText(self._textRect, self._text)
 
 
-class CallouPrice(CalloutAbs):
+class CallouPrice(CalloutChartAbs):
 
     def __init__(self, parent: QtCharts.QChart):
         super().__init__(parent)
@@ -249,10 +199,8 @@ class CallouPrice(CalloutAbs):
               painter: QPainter,
               option: QStyleOptionGraphicsItem,
               widget: QWidget):
-        #print("---------- paint ----------")
         path = QPainterPath()
-        mr = self._rect
-        path.addRoundedRect(mr, 5, 5)   # 丸みを帯びた長方形の角を規定
+        path.addRoundedRect(self._rect, 5, 5)   # 丸みを帯びた長方形の角を規定
 
         # 枠を描写
         painter.setBrush(CALLOUT_PRICE_COLOR)    # 図形の塗りつぶし
@@ -262,77 +210,6 @@ class CallouPrice(CalloutAbs):
         # 文字を描写
         painter.setPen(QPen(QColor(Qt.white)))
         painter.drawText(self._textRect, self._text)
-
-
-class CandlestickChartAbs(QtCharts.QChartView):
-    __metaclass__ = ABCMeta
-
-    COL_NAME_OP = "open"
-    COL_NAME_HI = "high"
-    COL_NAME_LO = "low"
-    COL_NAME_CL = "close"
-
-    def __init__(self, widget):
-        super().__init__()
-
-        # Chart Type
-        series = QtCharts.QCandlestickSeries()
-        series.setDecreasingColor(Qt.red)
-        series.setIncreasingColor(Qt.green)
-        #series.setCapsWidth(0.1)
-        #series.setBodyWidth(0.1)
-
-        # Create Chart and set General Chart setting
-        chart = QtCharts.QChart()
-        #chart.createDefaultAxes()
-        chart.layout().setContentsMargins(0, 0, 0, 0)
-        chart.setBackgroundRoundness(0)
-        chart.addSeries(series)
-
-        #Title Font size
-        """
-        font = QFont("Sans Serif", )
-        font.setPixelSize(18)
-        chart.setTitleFont(font)
-        """
-
-        #chart.setTitle("Temperature in Celcius For Device:")
-        chart.setAnimationOptions(QtCharts.QChart.SeriesAnimations)
-
-        palette = QPalette()
-        palette.setColor(QPalette.Text, Qt.red)
-        chart.setPalette(palette)
-
-        # Chart Background
-        """
-        backgroundGradient = QLinearGradient(0, 0, 0, 400)
-        backgroundGradient.setColorAt(0.0, QColor('#50a1dc'))
-        backgroundGradient.setColorAt(1.0, QColor('#00a1de'))
-        chart.setBackgroundBrush(backgroundGradient)
-        """
-
-        #Plot area background
-        plotAreaGradient = QLinearGradient(0, 100, 0, 400)
-        plotAreaGradient.setColorAt(0.0, QColor('#f1f1f1'))
-        plotAreaGradient.setColorAt(1.0, QColor('#ffffff'))
-        chart.setPlotAreaBackgroundBrush(plotAreaGradient)
-        chart.setPlotAreaBackgroundVisible(True)
-
-        chart.legend().hide()
-
-        self.setChart(chart)
-        self.setParent(widget)
-        self.resize(widget.frameSize())
-
-        chart.legend().setVisible(False)
-
-        self._series = series
-        self._chart = chart
-        self._widget = widget
-
-    @abstractmethod
-    def update(self, df):
-        raise NotImplementedError()
 
 
 class CandlestickChartGapFillPrev(CandlestickChartAbs):
