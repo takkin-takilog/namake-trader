d0dc175680776e3790b6d87a0993b1cafab324ae
takkin <takkin.takilog@gmail.com>
2020-07-19 00:55:07 +0900

    [update]

--- trade_monitor/trade_monitor/gapfill/gapfill_heatmap.py
+++ trade_monitor/trade_monitor/gapfill/gapfill_heatmap.py
@@ -24,22 +24,10 @@ from trade_monitor.abstract import CalloutChartAbs
 from trade_monitor import util as utl
 from trade_monitor.util import GradientManager
 from trade_monitor.util import INST_MSG_LIST
+from trade_monitor.gapfill.heatmap_manager import HeatMapManager
 
 import time
 
-COL_NAME_DATE = "date"
-COL_NAME_GPA_DIR = "gap dir"
-COL_NAME_GPA_CLOSE_PRICE = "gap close price"
-COL_NAME_GPA_OPEN_PRICE = "gap open price"
-COL_NAME_GPA_PRICE_MID = "gap price(mid)"
-COL_NAME_GPA_PRICE_REAL = "gap price(real)"
-COL_NAME_VALID_FLAG = "valid flag"
-COL_NAME_SUCCESS_FLAG = "success flag"
-COL_NAME_GAP_FILLED_TIME = "gap filled time"
-COL_NAME_MAX_OPEN_RANGE = "max open range"
-COL_NAME_END_CLOSE_PRICE = "end close price"
-
-COL_GPA_PRICE_TH = "gap price thresh"
 pd.set_option('display.max_columns', 1000)
 # pd.set_option('display.max_rows', 1000)
 
@@ -47,6 +35,20 @@ gradMng = GradientManager()
 
 
 def gen_sample_gapdata():
+
+    COL_NAME_DATE = "date"
+    COL_NAME_GPA_DIR = "gap dir"
+    COL_NAME_GPA_CLOSE_PRICE = "gap close price"
+    COL_NAME_GPA_OPEN_PRICE = "gap open price"
+    COL_NAME_GPA_PRICE_MID = "gap price(mid)"
+    COL_NAME_GPA_PRICE_REAL = "gap price(real)"
+    COL_NAME_VALID_FLAG = "valid flag"
+    COL_NAME_SUCCESS_FLAG = "success flag"
+    COL_NAME_GAP_FILLED_TIME = "gap filled time"
+    COL_NAME_MAX_OPEN_RANGE = "max open range"
+    COL_NAME_END_CLOSE_PRICE = "end close price"
+
+    COL_GPA_PRICE_TH = "gap price thresh"
 
     datalist = [
         ["2020-04-06", 0, 108.528, 108.382004, 0.145996, 0.096001, True, True, "7:00:00", 0.112999, 108.976997],
@@ -442,23 +444,14 @@ class HeatMapChartView(HeatMapChartViewAbs):
         callout.setGraphicsEffect(shadow)
 
         self.__sts_bar = sts_bar
-        self.__thin_num = 0
 
-    def rebuild_hmap(self, df: pd.DataFrame, thin_num: int):
+    def rebuild_hmap(self, df_hmap: pd.DataFrame):
 
-        df_s = df.sort_values(df.index.name, ascending=True)
+        self.__draw_map(df_hmap)
 
-        df_t = self.__thin_out_map(df_s, thin_num)
-        self.__draw_map(df_t)
+    def update_hmap(self, df_hmap: pd.DataFrame):
 
-        self.__thin_num = thin_num
-
-    def update_hmap(self, df: pd.DataFrame):
-
-        df_s = df.sort_values(df.index.name, ascending=True)
-
-        df_t = self.__thin_out_map(df_s, self.__thin_num)
-        self.__update_hmap(df_t)
+        self.__update_hmap(df_hmap)
 
     def update_color(self):
         for block in self.chart().series():
@@ -490,59 +483,6 @@ class HeatMapChartView(HeatMapChartViewAbs):
         self.resize(fs)
     """
 
-    def __thin_out_map(self, df: pd.DataFrame, thin_num: int):
-
-        # print("---------- thin_out_map ----------")
-        #print(df)
-        #print(thin_num)
-
-        if thin_num < 2:
-            return df
-
-        col_min = df.columns[0]
-        col_max = df.columns[-1]
-        row_min = df.index[0]
-        row_max = df.index[-1]
-
-        rng_y = list(range(((row_min - 1) // thin_num) * thin_num + thin_num,
-                           row_max + thin_num,
-                           thin_num))
-
-        rng_x = list(range(((col_min - 1) // thin_num) * thin_num + thin_num,
-                           col_max + thin_num,
-                           thin_num))
-
-        #print("rng_x: {}" .format(rng_x))
-        #print("rng_y: {}" .format(rng_y))
-
-        self.__sts_bar.set_label_text("[1/3]")
-        self.__sts_bar.set_bar_range(0, len(rng_y) * len(rng_x))
-
-        new_y_map = []
-        cnt = 0
-        for y in rng_y:
-            str_y = y - thin_num + 1
-            str_y = utl.limit(str_y, row_min, row_max)
-            end_y = utl.limit(y, row_min, row_max) + 1
-            y_rng = range(str_y,  end_y, 1)
-            new_x_list = [y]
-            for x in rng_x:
-                str_x = x - thin_num + 1
-                str_x = utl.limit(str_x, col_min, col_max)
-                end_x = utl.limit(x, col_min, col_max) + 1
-                x_rng = range(str_x,  end_x, 1)
-                new_x_list.append(df.loc[y_rng][x_rng].max().max())
-                cnt = cnt + 1
-                self.__sts_bar.set_bar_value(cnt)
-            new_y_map.append(new_x_list)
-
-        idx = "Y"
-        columns = [idx] + list(rng_x)
-        df_new = pd.DataFrame(new_y_map, columns=columns)
-        df_new.set_index(idx, inplace=True)
-        # print(df_new)
-
-        return df_new
 
     def __draw_map(self, df: pd.DataFrame):
 
@@ -572,7 +512,6 @@ class HeatMapChartView(HeatMapChartViewAbs):
         # print("-------------- diff:{}" .format(diff))
 
         self.__sts_bar.set_label_text("[2/3]")
-        time_start = time.time()
         if 0 < diff:
             # print("===== 0 < diff =====")
             self.__sts_bar.set_bar_range(0, diff)
@@ -591,9 +530,6 @@ class HeatMapChartView(HeatMapChartViewAbs):
                 srlist = chart.series()
                 chart.removeSeries(srlist[-1])
                 self.__sts_bar.set_bar_value(i + 1)
-
-        elapsed_time = time.time() - time_start
-        print ("elapsed_time:{0}".format(elapsed_time) + "[sec]")
 
         self.__sts_bar.set_label_text("[3/3]")
         self.__sts_bar.set_bar_range(0, df.size)
@@ -713,13 +649,11 @@ class GapFillHeatMap(QMainWindow):
         callback = self.__on_scrollBarDate_changed
         ui.scrollBar_Date.valueChanged.connect(callback)
 
+        self.__hmapmng = HeatMapManager(sts_bar)
+
         self.__ui = ui
         self.__chart_view = chart_view
         self.__color_map = color_map
-        self.__inst_idx = 0
-
-        self.__df_param = pd.DataFrame()
-        self.__df_htbl = pd.DataFrame()
 
     def __on_spinBoxDateStep_changed(self, value):
         print("spinBoxDateStep_value: {}" .format(value))
@@ -730,17 +664,11 @@ class GapFillHeatMap(QMainWindow):
     def __on_scrollBarDate_changed(self, value):
         print("scrollBarDate_value: {}" .format(value))
         if self.__ui.pushButton_AutoUpdate.isChecked():
-            self.__update_hmap(self.__df_hmap)
+            self.__update_hmap()
 
-    def __update_hmap(self, df_hmap: pd.DataFrame):
-        df_hmap_new = self.__remake_df_hmap(df_hmap)
-        self.__chart_view.update_hmap(df_hmap_new)
-
-    def __remake_df_hmap(self, df_hmap: pd.DataFrame):
-
-        df_hmap_new = df_hmap
-
-        return df_hmap_new
+    def __update_hmap(self):
+        df = self.__hmapmng.tuned_hmap()
+        self.__chart_view.update_hmap(df)
 
     def __on_pushButtonGenHMap_clicked(self):
         # 以下はテストコード
@@ -752,101 +680,28 @@ class GapFillHeatMap(QMainWindow):
         print("--------------- df comp --------------------")
         """
         self.__ui.pushButton_AutoUpdate.setChecked(False)
-        self.rebuild_hmap(self.__df_hmap)
 
-        step_val = len(self.__df_param)
+        deci = self.__ui.spinBox_ThinOut.value()
+        df = self.__hmapmng.decimate_hmap(deci)
+
+        self.__chart_view.rebuild_hmap(df)
+        self.__color_map.update_intensity_range()
+
+        step_val = self.__hmapmng.param_len
         self.__ui.spinBox_DateStep.setValue(step_val)
         self.__ui.spinBox_DateStep.setMaximum(step_val)
         self.__ui.scrollBar_Date.setMaximum(0)
 
     def __on_pushButton_test_clicked(self):
         df_param, inst_idx = gen_sample_gapdata()
-        self.set_gapfill_param(df_param, inst_idx)
 
-    def set_gapfill_param(self, df_param: pd.DataFrame, inst_idx):
-        df_valid = df_param[df_param[COL_NAME_VALID_FLAG]]
+        self.__hmapmng.set_param(df_param, inst_idx)
 
-        # print("---------- df_valid ----------")
-        # print(df_valid)
-        # print("---------- df_valid ----------")
-        max_open_price_max = df_valid[COL_NAME_MAX_OPEN_RANGE].max()
-        # print("---------- max_open_price_max: {}" .format(max_open_price_max))
-        decimal_digit = INST_MSG_LIST[inst_idx].decimal_digit
-        lsb = math.pow(10, decimal_digit)
+        shape = self.__hmapmng.shape
+        lenmax = max(shape)
+        val = (lenmax // 100) + 1
 
-        margin = 5
-        max_open_pips_max = utl.roundi(max_open_price_max * lsb) + margin
-        # print("---------- max_open_pips_max: {}" .format(max_open_pips_max))
-        hmap_col = [COL_NAME_DATE] + list(range(1, max_open_pips_max + 1))
-        # print("---------- hmap_col: {}" .format(hmap_col))
-
-        roslist = []
-        for date, is_succ, mop, gpr in zip(df_valid.index,
-                                           df_valid[COL_NAME_SUCCESS_FLAG],
-                                           df_valid[COL_NAME_MAX_OPEN_RANGE],
-                                           df_valid[COL_NAME_GPA_PRICE_REAL]):
-            if is_succ:
-                max_open_pips = utl.roundi(mop * lsb)
-            else:
-                max_open_pips = max_open_pips_max
-
-            row_left = list(range(-1, -max_open_pips-1, -1))
-            gap_pips = utl.roundi(gpr * lsb)
-
-            row_right = [gap_pips] * (max_open_pips_max - max_open_pips)
-            roslist.append([date] + row_left + row_right)
-
-        df_htbl = pd.DataFrame(roslist, columns=hmap_col)
-        df_htbl.set_index(COL_NAME_DATE, inplace=True)
-        df_hmap = self.__make_hmap(df_param, df_htbl, inst_idx)
-
-        lenmax = max(df_hmap.shape)
-        thinout = (lenmax // 100) + 1
-
-        self.__df_param = df_param
-        self.__df_htbl = df_htbl
-        self.__inst_idx = inst_idx
-        self.__df_hmap = df_hmap
-
-        self.__ui.spinBox_ThinOut.setValue(thinout)
-
-    def __make_hmap(self,
-                    df_param: pd.DataFrame,
-                    df_htbl: pd.DataFrame,
-                    inst_idx: int
-                    ):
-
-        gap_price_real_max = df_param[COL_NAME_GPA_PRICE_REAL].max()
-        # print("---------- gap_price_real_max: {}" .format(gap_price_real_max))
-        decimal_digit = INST_MSG_LIST[inst_idx].decimal_digit
-        lsb = math.pow(10, decimal_digit)
-
-        margin = 5
-        gap_pips_max = utl.roundi(gap_price_real_max * lsb) + margin
-        # print("---------- gap_pips_max: {}" .format(gap_pips_max))
-
-        # print("---------- column: {}" .format(df_htbl.columns))
-
-        df_mst = pd.DataFrame()
-        for date, htbl in df_htbl.iterrows():
-            gap_price = df_param.loc[date][COL_NAME_GPA_PRICE_REAL]
-            gap_pips = utl.roundi(gap_price * lsb)
-            collist = [htbl.to_list()] * (gap_pips_max - gap_pips)
-            # print("---------- collist: {}" .format(len(collist)))
-            gpt_col = list(range(gap_pips + 1, gap_pips_max + 1))
-
-            df = pd.DataFrame(collist,
-                              columns=df_htbl.columns)
-            df[COL_GPA_PRICE_TH] = gpt_col
-            df[COL_NAME_DATE] = date
-            df.set_index([COL_NAME_DATE, COL_GPA_PRICE_TH], inplace=True)
-            # print("===== df =====")
-            # print(df)
-            df_mst = pd.concat([df_mst, df])
-        # df_mst.sort_index(inplace=True)
-        # print(df_mst)
-
-        return df_mst.sum(level=COL_GPA_PRICE_TH).sort_index()
+        self.__ui.spinBox_ThinOut.setValue(val)
 
     def __on_gradientBtoYPB_clicked(self):
         grBtoY = QLinearGradient(0, 0, 0, 100)
@@ -870,21 +725,13 @@ class GapFillHeatMap(QMainWindow):
         self.__chart_view.update_color()
         self.__color_map.update_color_scale()
 
-    def __on_spinBoxThinOut_changed(self, i):
+    def __on_spinBoxThinOut_changed(self, thinout):
         # print("========= __on_spinBoxThinOut_changed =========")
-        self.__update_hmap_size_txt(self.__df_hmap, i)
-
-    def __update_hmap_size_txt(self, df_hmap, thinout):
-        row_size = df_hmap.shape[0] // thinout
-        col_size = df_hmap.shape[1] // thinout
+        shape = self.__hmapmng.shape
+        row_size = shape[0] // thinout
+        col_size = shape[1] // thinout
         txt = "行数：" + str(row_size) + "\n列数：" + str(col_size)
         self.__ui.label_Roughness.setText(txt)
-
-    def rebuild_hmap(self, df: pd.DataFrame):
-
-        thin_num = self.__ui.spinBox_ThinOut.value()
-        self.__chart_view.rebuild_hmap(df, thin_num)
-        self.__color_map.update_intensity_range()
 
     def __load_ui(self, parent):
         loader = QUiLoader()
---
+++ trade_monitor/trade_monitor/gapfill/heatmap_manager.py
@@ -0,0 +1,163 @@
+import math
+import pandas as pd
+from trade_monitor.util import INST_MSG_LIST
+from trade_monitor import util as utl
+
+COL_NAME_DATE = "date"
+COL_NAME_GPA_DIR = "gap dir"
+COL_NAME_GPA_CLOSE_PRICE = "gap close price"
+COL_NAME_GPA_OPEN_PRICE = "gap open price"
+COL_NAME_GPA_PRICE_MID = "gap price(mid)"
+COL_NAME_GPA_PRICE_REAL = "gap price(real)"
+COL_NAME_VALID_FLAG = "valid flag"
+COL_NAME_SUCCESS_FLAG = "success flag"
+COL_NAME_GAP_FILLED_TIME = "gap filled time"
+COL_NAME_MAX_OPEN_RANGE = "max open range"
+COL_NAME_END_CLOSE_PRICE = "end close price"
+
+COL_GPA_PRICE_TH = "gap price thresh"
+
+
+class HeatMapManager():
+
+    def __init__(self, sts_bar):
+        self.__sts_bar = sts_bar
+
+    def set_param(self, df_param: pd.DataFrame, inst_idx):
+
+        df_valid = df_param[df_param[COL_NAME_VALID_FLAG]]
+
+        max_open_price_max = df_valid[COL_NAME_MAX_OPEN_RANGE].max()
+
+        decimal_digit = INST_MSG_LIST[inst_idx].decimal_digit
+        lsb = math.pow(10, decimal_digit)
+
+        margin = 5
+        max_open_pips_max = utl.roundi(max_open_price_max * lsb) + margin
+        # print("---------- max_open_pips_max: {}" .format(max_open_pips_max))
+        hmap_col = [COL_NAME_DATE] + list(range(1, max_open_pips_max + 1))
+        # print("---------- hmap_col: {}" .format(hmap_col))
+
+        roslist = []
+        for date, is_succ, mop, gpr in zip(df_valid.index,
+                                           df_valid[COL_NAME_SUCCESS_FLAG],
+                                           df_valid[COL_NAME_MAX_OPEN_RANGE],
+                                           df_valid[COL_NAME_GPA_PRICE_REAL]):
+            if is_succ:
+                max_open_pips = utl.roundi(mop * lsb)
+            else:
+                max_open_pips = max_open_pips_max
+
+            row_left = list(range(-1, -max_open_pips - 1, -1))
+            gap_pips = utl.roundi(gpr * lsb)
+
+            row_right = [gap_pips] * (max_open_pips_max - max_open_pips)
+            roslist.append([date] + row_left + row_right)
+
+        df_htbl = pd.DataFrame(roslist, columns=hmap_col)
+        df_htbl.set_index(COL_NAME_DATE, inplace=True)
+
+        df_hmap_mst = self.__make_hmap(df_param, df_htbl, inst_idx)
+        df_hmap = df_hmap_mst.sum(level=COL_GPA_PRICE_TH).sort_index()
+
+        df_hmap_zero = pd.DataFrame(index=df_hmap.index,
+                                    columns=df_hmap.columns).fillna(0)
+
+        self.__df_param = df_param
+        self.__df_htbl = df_htbl
+        self.__inst_idx = inst_idx
+        self.__df_hmap_mst = df_hmap_mst
+        self.__df_hmap = df_hmap
+        self.__df_hmap_zero = df_hmap_zero
+        self.__decimate_value = 0
+
+    def decimate_hmap(self, deci: int):
+
+        if deci < 2:
+            return self.__df_hmap
+
+        df = self.__df_hmap
+
+        col_min = df.columns[0]
+        col_max = df.columns[-1]
+        row_min = df.index[0]
+        row_max = df.index[-1]
+
+        rng_y = list(range(((row_min - 1) // deci) * deci + deci,
+                           row_max + deci,
+                           deci))
+
+        rng_x = list(range(((col_min - 1) // deci) * deci + deci,
+                           col_max + deci,
+                           deci))
+
+        self.__sts_bar.set_label_text("[1/3]")
+        self.__sts_bar.set_bar_range(0, len(rng_y) * len(rng_x))
+
+        new_y_map = []
+        cnt = 0
+        for y in rng_y:
+            str_y = y - deci + 1
+            str_y = utl.limit(str_y, row_min, row_max)
+            end_y = utl.limit(y, row_min, row_max) + 1
+            y_rng = range(str_y,  end_y, 1)
+            new_x_list = [y]
+            for x in rng_x:
+                str_x = x - deci + 1
+                str_x = utl.limit(str_x, col_min, col_max)
+                end_x = utl.limit(x, col_min, col_max) + 1
+                x_rng = range(str_x,  end_x, 1)
+                new_x_list.append(df.loc[y_rng][x_rng].max().max())
+                cnt = cnt + 1
+                self.__sts_bar.set_bar_value(cnt)
+            new_y_map.append(new_x_list)
+
+        idx = "Y"
+        columns = [idx] + list(rng_x)
+        df_new = pd.DataFrame(new_y_map, columns=columns)
+        df_new.set_index(idx, inplace=True)
+
+        self.__decimate_value = deci
+
+        return df_new
+
+    def tuned_hmap(self):
+
+        return self.decimate_hmap(self.__decimate_value)
+
+    def __make_hmap(self,
+                    df_param: pd.DataFrame,
+                    df_htbl: pd.DataFrame,
+                    inst_idx: int
+                    ):
+
+        gap_price_real_max = df_param[COL_NAME_GPA_PRICE_REAL].max()
+        decimal_digit = INST_MSG_LIST[inst_idx].decimal_digit
+        lsb = math.pow(10, decimal_digit)
+
+        margin = 5
+        gap_pips_max = utl.roundi(gap_price_real_max * lsb) + margin
+
+        df_mst = pd.DataFrame()
+        for date, htbl in df_htbl.iterrows():
+            gap_price = df_param.loc[date][COL_NAME_GPA_PRICE_REAL]
+            gap_pips = utl.roundi(gap_price * lsb)
+            collist = [htbl.to_list()] * (gap_pips_max - gap_pips)
+            gpt_col = list(range(gap_pips + 1, gap_pips_max + 1))
+
+            df = pd.DataFrame(collist,
+                              columns=df_htbl.columns)
+            df[COL_GPA_PRICE_TH] = gpt_col
+            df[COL_NAME_DATE] = date
+            df.set_index([COL_NAME_DATE, COL_GPA_PRICE_TH], inplace=True)
+            df_mst = pd.concat([df_mst, df])
+
+        return df_mst
+
+    @property
+    def shape(self):
+        return self.__df_hmap.shape
+
+    @property
+    def param_len(self):
+        return len(self.__df_param)
